b9acfd4b5804c0fce21d3193208aa07c13b19d78
matheus.uchoa <matheus.uchoa@azi.com.br>
2026-02-05 22:48:00 -0400

    [CONFIG] Ajusta configs de cors

--- teste-api/src/main/java/com/example/config/CorsFilter.java
+++ teste-api/src/main/java/com/example/config/CorsFilter.java
@@ -6,14 +6,23 @@ import jakarta.ws.rs.container.ContainerResponseFilter;
 import jakarta.ws.rs.ext.Provider;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 @Provider
 public class CorsFilter implements ContainerResponseFilter {
-
+    private final List<String> allowedOrigins = Arrays.asList(
+            "https://teste-projedata-front.onrender.com",
+            "http://localhost:5173"
+    );
     @Override
     public void filter(ContainerRequestContext requestContext,
                        ContainerResponseContext responseContext) throws IOException {
-        responseContext.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:5173");
+        String origin = requestContext.getHeaderString("Origin");
+
+        if (origin != null && allowedOrigins.contains(origin)) {
+            responseContext.getHeaders().add("Access-Control-Allow-Origin", origin);
+        }
         responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
         responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
         responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
